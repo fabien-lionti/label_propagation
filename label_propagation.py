a991088a3047492c0e852dc24e4831883a2f4240
fabien-lionti <fabien.lionti@gmail.com>
2022-04-30 18:48:34 +0200

    update get_data

--- label_propagation.py
+++ label_propagation.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_moons
 import numpy as np
+from scipy.stats import entropy
 
-def get_data(nb_data, pct_unlabelled):
-    labels = [0, 1]
-    x, y = make_moons(n_samples=200, shuffle=True, noise=0.1, random_state=None)
+def get_data(nb_data, pct_unlabelled, nb_class=2):
+    labels = list(np.arange(0,nb_class))
+    x, y = make_moons(n_samples=nb_data, shuffle=True, noise=0.1, random_state=None)
     y_true = np.copy(y)
     for label in labels:
         selected_idx_labels = np.where(y_true == label)[0]
         unlabelled_idx = np.random.choice(
             selected_idx_labels, 
-            int(nb_data * (1 - pct_unlabelled / 2)), replace=False)
+            int((nb_data / nb_class) * (1 - pct_unlabelled)), replace=False)
         y[unlabelled_idx] = -1
     return x, y, y_true
 
-def plot_data(x, y, y_true):
+def plot_data(x, y, y_true, confidence = None):
+    if confidence is None : confidence = [1] * len(y)
+
     colors = [[0.4, 0.4, 0.4], [1, 0, 0], [0, 1, 0]]
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (20, 10))
     for i in range(len(y)):
-        ax1.plot(x[i,0], x[i,1], '.', c=colors[y[i]+1], markersize=10)
+        ax1.plot(x[i,0], x[i,1], '.', c=colors[y[i]+1], markersize=10 * confidence[i])
         ax2.plot(x[i,0], x[i,1], '.', c=colors[y_true[i]+1], markersize=10)
     ax1.set_title('Moon with unlabelled data')
     ax1.grid()
@@ -51,16 +54,31 @@ def get_label_encoding_matrix(y):
             Y[i] = np.ones(2) / 2
     return Y
 
-def label_propagation(T, Y, nb_iter=100):
+def label_propagation(x, T, Y, nb_iter=100, plot_propagation=False):
+    nb_class = Y.shape[-1]
+    confidence = np.zeros(Y.shape[0])
     for _ in range(nb_iter):
         Y =  T @ Y
         for i ,label in enumerate(y):
             if label != -1:
-                c = np.zeros(2)
+                c = np.zeros(nb_class)
                 c[int(label)] = 1.0
                 Y[i] = c
             else:
                 Y[i] / Y[i].sum()
+
+            confidence[i] = entropy(Y[i]) / entropy([1/nb_class] * nb_class)
+
+        if plot_propagation:   
+            y_pred = np.argmax(Y, axis=1)
+            colors = np.asarray([[0.4, 0.4, 0.4], [1.0, 0, 0], [0, 1.0, 0]])
+            plt.figure(figsize = (10, 10))
+            for j in range(len(y_pred)):
+                plt.plot(x[j,0], x[j,1], '.', c=colors[y_pred[j]+1], markersize=10 * confidence[j])
+            plt.grid()
+            #plt.show()
+            #plt.savefig(f'./tmp/{_}.png')
+
     y_pred = np.argmax(Y, axis=1)
     return y_pred
 
@@ -71,9 +89,9 @@ def get_acc(y_pred, y_true):
 if __name__ == '__main__':
 
     # Set parameters
-    nb_data = 100
+    nb_data = 400
     pct_unlabelled = 0.01
-    sigma = 0.2
+    sigma = 0.1
     nb_iter = 100
 
     # Get dataset
@@ -85,7 +103,7 @@ if __name__ == '__main__':
     weighted_adjacency_matrix = get_rbf(distance_matrix, sigma=sigma)
     T = get_transition_matrix(weighted_adjacency_matrix)
     Y = get_label_encoding_matrix(y)
-    y_pred = label_propagation(T, Y, nb_iter=nb_iter)
+    y_pred = label_propagation(x, T, Y, nb_iter=nb_iter)
 
     # Get results
     unlabelled_mask = y == -1
